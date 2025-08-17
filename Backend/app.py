e602b82de1aa4845c017e47cf2ab5dd0cf583dfb
deibfps <id1386859@gmail.com>
2025-08-17 07:23:38 -0600

    Se inicializo el backend con Python y Flask y se agrego el .gitignore

---
+++ Backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")  # Solo el nombre del archivo
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
